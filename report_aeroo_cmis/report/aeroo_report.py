b886d77544f80eb625d8e6165d694a74fb4d9ddb
Laurent Mignon <laurent.mignon@acsone.eu>
2016-02-21 22:42:46 +0100

    [IMP] WIP  #18140

--- report_aeroo_cmis/report/aeroo_report.py
+++ report_aeroo_cmis/report/aeroo_report.py
@@ -3,9 +3,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import time
+import os.path
 
+import openerp
 from openerp.exceptions import Warning
 from openerp.tools.safe_eval import safe_eval
+from openerp.addons.cmis.models.cmis_folder import CmisFolder
 from openerp.addons.report_aeroo import report_aeroo
 
 
@@ -19,6 +22,33 @@ class Aeroo_report(report_aeroo.Aeroo_report):
             cr, uid, ids, data, report_xml, context=context)
         obj = self. getObjects_mod(
             cr, uid, ids, report_xml.report_type, context=context)[0]
-        if obj.cmis_filename:
-            name = safe_eval(obj.cmis_filename, {'object': obj, 'time': time})
+        if report_xml.cmis_filename:
+            with openerp.api.Environment.manage():
+                env = openerp.api.Environment(cr, uid, context).sudo()
+                report = env[report_xml._name].browse(report_xml.id)
+                self.store_in_cmis(
+                    env, report, env[obj._name].browse(obj.id), res)
         return res
+
+    def store_in_cmis(self, env, report_xml, obj, res):
+        name = safe_eval(
+            report_xml.cmis_filename, {'object': obj, 'time': time})
+        if not name:
+            return
+        pdf = res[0]
+        cmis_parent_objectid = None
+        # if the linked object is CmisFolder, the report will be
+        # stored as child of the referenced cmis object otherwise the report
+        # will be saved as child of the repository
+        if isinstance(obj, CmisFolder):
+            cmis_parent_objectid = obj.cmis_objectid
+            backend = obj.backend_id
+        else:
+            backend = report_xml.backend_id
+        if not backend:
+            raise Warning("No CMIS Backend configured")
+        path = os.path.dirname(name) or '/'
+        target_folder_objectid = backend.get_folder_by_path(
+            path, create_if_not_found=True,
+            cmis_parent_objectid=cmis_parent_objectid)
+            #backend 
