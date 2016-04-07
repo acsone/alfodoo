004046315e09a9601a1994ed68f49e7fd6bc4987
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2016-04-07 18:26:42 +0200

    [IMP] report_aeroo comeback

---
+++ report_aeroo_cmis/models/ir_actions_report_xml.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openerp import api, fields, models
+from openerp.tools.translate import _
+from openerp.report import interface
+
+from ..report import Aeroo_report
+
+
+class IrActionsReportXml(models.Model):
+    _name = 'ir.actions.report.xml'
+    _inherit = 'ir.actions.report.xml'
+
+    cmis_filename = fields.Char(
+        'Save in CMIS Prefix',
+        help='This is the filename of the attachment used to store the '
+             'printing result. Keep empty to not save the printed reports. '
+             'You can use a python expression with the object and time '
+             'variables. If your name contains path separators "/", the file '
+             'will be stored into sthe specified subdirectory. (missing path'
+             'elements are created if not found). The path is always '
+             'interpreted as subpath of of the initial_directory_write '
+             'defined on the cmis.bakend if the related object is not a '
+             'cmis.folder, otherwise as a subpath of the cmis folder linked'
+             'to the associated object of the report.')
+    backend_id = fields.Many2one(
+        comodel_name="cmis.backend",
+        string="Backend",
+        help='The backend to use if your report is not linked to an object or'
+             'the linked object is not a cmis folder')
+    cmis_duplicate_handler = fields.Selection(
+        selection=[
+            ('error',       _('Raise exception')),
+            ('new_version', _('Create a new version')),
+            ('increment',   _('Rename as file(X).pdf'))
+        ],
+        string='Strategy in case of duplicate',
+        default='error')
+
+    @api.model
+    def register_report(self, name, model, tmpl_path, parser):
+        name = 'report.%s' % name
+        if name in interface.report_int._reports:
+            del interface.report_int._reports[name]
+        res = Aeroo_report(self.env.cr, name, model, tmpl_path, parser=parser)
+        return res
---
+++ report_aeroo_cmis_alf/README.rst
@@ -0,0 +1,46 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+    :alt: License: AGPL-3
+
+=====================
+report_aeroo_cmis_alf
+=====================
+
+Once installed, when generating an aeroo report saved into Alfresco, the user is redirected
+to the page in Alfresco share displaying the details of the generated report
+
+Configuration
+=============
+
+To configure this module, you need to:
+
+ * go to ...
+
+Usage
+=====
+
+To use this module, you need to:
+
+ * go to ...
+
+Known issues / Roadmap
+======================
+
+ * ...
+
+Credits
+=======
+
+Contributors
+------------
+
+* Laurent Mignon <laurent.mignon@acsone.eu>
+
+Maintainer
+----------
+
+.. image:: https://www.acsone.eu/logo.png
+   :alt: ACSONE SA/NV
+   :target: http://www.acsone.eu
+
+This module is maintained by ACSONE SA/NV.
