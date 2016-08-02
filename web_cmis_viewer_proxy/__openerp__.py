a470731d76586a6945b9c9d0c16298ca835c18de
Laurent Mignon <laurent.mignon@acsone.eu>
2016-08-02 15:06:36 +0200

    [ADD] web_cmis_viewer_proxy

---
+++ web_cmis_viewer_proxy/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Web Cmis Viewer Proxy',
+    'summary': """
+        Odoo as proxy server for your cmis requests from the web to the cmis container.""",
+    'version': '9.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
+    'website': 'https://acsone.eu/',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
