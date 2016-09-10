2f457898bb69188aedc8914a25f201c625a12d85
Laurent Mignon <laurent.mignon@acsone.eu>
2016-09-10 13:09:55 +0200

    rename web_cmis_viewer_proxy cmis_web_proxy

---
+++ cmis_web_proxy/models/cmis_backend.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, fields, models, _
+from openerp import tools
+
+class CmisBackend(models.Model):
+
+    _inherit = 'cmis.backend'
+
+    @tools.cache()
+    def get_default_backend(self):
+        res = self.search([(1, '=', 1)])
+        res.ensure_one()
+        return res
+
+    @api.multi
+    def write(self, vals):
+        self.get_default_backend.clear_cache(self)
+        if 'is_cmis_proxy' in vals and \
+            vals['is_cmis_proxy'] == False:
+            vals['apply_odoo_security'] = False
+        return super(CmisBackend, self).write(vals)
+
+    @api.onchange('is_cmis_proxy')
+    def _onchange_is_cmis_proxy(self):
+        self.apply_odoo_security = self.is_cmis_proxy
+
+    is_cmis_proxy = fields.Boolean(
+        required=True, default=False,
+        help=_("If checked, all the CMIS requests from the client will be "
+               "done to the Odoo server in place of to a direct call to the"
+               "CMIS Container. In such a case, Odoo act as a proxy server "
+               "between the widget and the cmis container and all the requets "
+               "are done by using the configured account on the backend. "))
+    apply_odoo_security = fields.Boolean(
+        required=True, default=False, 
+        help=_("If checked, the Odoo security rules are applied to the "
+               "content retrieved from the cmis container and the available "
+               "actions on this content."))
