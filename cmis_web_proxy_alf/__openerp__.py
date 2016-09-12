e0dc98d59d2d179550a5e88a97b571be7b3591bb
Laurent Mignon <laurent.mignon@acsone.eu>
2016-09-12 16:14:02 +0200

    Add new modul cmis_web_proxy_alf

---
+++ cmis_web_proxy_alf/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Cmis Web Proxy Alf',
+    'description': """
+        Proxy requests from cmis_web to the Alfresco API""",
+    'version': '9.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'ACSONE SA/NV',
+    'website': 'https://acsone.eu/',
+    'depends': [
+        'cmis_web_proxy'
+    ],
+    'data': [
+        'views/cmis_backend.xml',
+        'views/cmis_web_proxy_alf.xml'
+    ],
+    'demo': [
+    ],
+}
