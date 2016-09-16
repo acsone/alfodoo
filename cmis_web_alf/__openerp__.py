9a86f6a8bd899f94beb00e2d1e413ce237dc48d7
Laurent Mignon <laurent.mignon@acsone.eu>
2016-09-16 12:39:52 +0200

    Translations

--- cmis_web_alf/__openerp__.py
+++ cmis_web_alf/__openerp__.py
@@ -2,9 +2,9 @@
 # Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': "Alfresco CMIS Viewer Widget",
+    'name': "Alfresco CMIS Web interface",
     'summary': """
-        Alfresco extension for the CMIS viewer widget""",
+        Alfresco extension for the CMIS web widgets""",
     'author': 'ACSONE SA/NV',
     'website': "http://alfodoo.org",
     'category': 'Uncategorized',
