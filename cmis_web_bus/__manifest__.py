56ed93849f05a335dca5d76e59c89b973f1fae7a
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2017-02-28 16:16:59 +0100

    Typo

--- cmis_web_bus/__manifest__.py
+++ cmis_web_bus/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': 'Cmis Web Bus',
     'description': """
-        Instant Messaging Bos to send notification to the CMIS components
+        Instant Messaging Bus to send notification to the CMIS components
         in live""",
     'version': '10.0.1.0.0',
     'license': 'AGPL-3',
