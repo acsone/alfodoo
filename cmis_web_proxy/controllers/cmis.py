9fbd00f6801bcb4377ccb33e8c961060822a1bf3
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2018-02-01 17:39:15 +0100

    [IMP] cmis_proxy: Improve modularity of _check_access_operation It's now possible to define a method '_check_cmis_access_operation' on the model to adapt the _check_access_operation behaviour. If the method return True or False, the normal behaviour is skipped. If None, the check fallback on its normal impementation

--- cmis_web_proxy/controllers/cmis.py
+++ cmis_web_proxy/controllers/cmis.py
@@ -136,7 +136,31 @@ class CmisProxy(http.Controller):
                 values[k] = v.replace(original, new)
 
     def _check_access_operation(self, model_inst, operation):
+        """
+        Check if the user has the appropriate rights to perform the operation.
+        The default is to check the access rights and access rules on the
+        model instance. This behaviour can be adapted by defining the method
+        ''_check_cmis_access_operation'' on the model.
+        ::
+            @api.multi
+            def _check_cmis_access_operation(self, operation, field_name=None):
+                if my_true_condition:
+                    return True
+                if my_false_condition:
+                     return False
+                return None
+
+        The expected result must be in (True, False, None).
+        * True: Access granted
+        * False: Access Denied
+        * None: The current method will check the access rights and access
+                rules
+        """
         try:
+            if hasattr(model_inst, '_check_cmis_access_operation'):
+                res = model_inst._check_cmis_access_operation(operation, None)
+                if res is not None:
+                    return res
             model_inst.check_access_rights(operation)
             model_inst.check_access_rule(operation)
         except AccessError:
