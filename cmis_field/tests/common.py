97bf9c1385e7c48ac7e0f777008ef12a429d2435
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2018-08-03 17:14:20 +0200

    [FIX] cmis_field: Add support for cmis_folder defined as related

--- cmis_field/tests/common.py
+++ cmis_field/tests/common.py
@@ -28,7 +28,15 @@ class BaseTestCmis(common.SavepointCase):
         super(BaseTestCmis, cls).setUpClass()
         # mock commit since it"s called in the _auto_init method
         cls.cr.commit = mock.MagicMock()
-        cls.cmis_test_model = cls._init_test_model(models.CmisTestModel)
+        cls.cmis_test_model = cls._init_test_model(
+            models.CmisTestModel
+        )
+        cls.cmis_test_model_inherits = cls._init_test_model(
+            models.CmisTestModelInherits
+        )
+        cls.cmis_test_model_related = cls._init_test_model(
+            models.CmisTestModelRelated
+        )
         cls.cmis_backend = cls.env.ref('cmis.cmis_backend_alfresco')
         cls.cmis_backend.initial_directory_write = '/odoo'
 
