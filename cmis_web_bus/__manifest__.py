00b42e505ccd8bd36ecc2571bb85e2e79a180c76
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-11-20 13:46:47 +0100

    [ADD] cmis_web_bus: Migration to 13.0 First official release of this addons developped initialiy for odoo 9.0

--- cmis_web_bus/__manifest__.py
+++ cmis_web_bus/__manifest__.py
@@ -1,23 +1,16 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Cmis Web Bus',
-    'description': """
+    "name": "Cmis Web Bus",
+    "description": """
         Instant Messaging Bus to send notification to the CMIS components
         in live""",
-    'version': '10.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV',
-    'website': 'https://acsone.eu/',
-    'depends': [
-        'cmis_web',
-        'bus',
-    ],
-    'data': [
-        'views/cmis_web_bus.xml'
-    ],
-    'demo': [
-    ],
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu/",
+    "depends": ["cmis_web", "bus"],
+    "data": ["views/cmis_web_bus.xml"],
+    "demo": [],
 }
