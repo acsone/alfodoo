74ccb6cd4acece93d5292d79b5512b9615d89599
Arnaud Pineux <arnaud.pineux@acsone.eu>
2021-06-29 14:43:15 +0200

    [MIG] cmis_web_report_write_alf: Migration to 14.0

--- cmis_web_report_write_alf/__manifest__.py
+++ cmis_web_report_write_alf/__manifest__.py
@@ -5,12 +5,12 @@
     "name": "Cmis Web Report Write Alf",
     "description": """
         Automatically open generated editable reports into Alfresco""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://acsone.eu/",
     "depends": ["cmis_web_report_write", "cmis_web_bus"],
     "data": ["views/cmis_web_report_write_alf.xml"],
     "demo": [],
-    "installable": False,
+    "installable": True,
 }
