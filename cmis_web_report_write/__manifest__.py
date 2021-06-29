7f6691f09f93b4aa4b4cb323ce2e1ec2d36b399d
Arnaud Pineux <arnaud.pineux@acsone.eu>
2021-06-29 14:43:11 +0200

    [MIG] cmis_web_report_write: Migration to 14.0

--- cmis_web_report_write/__manifest__.py
+++ cmis_web_report_write/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'Cmis Web Report Write',
     'description': """
         Notify CMIS web component on report saved into CMIS""",
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV',
     'website': 'https://acsone.eu/',
@@ -17,5 +17,5 @@
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
 }
