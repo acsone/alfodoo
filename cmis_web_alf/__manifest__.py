32e5616b3914d2f9567a62d1ce5e76aff7e29d0e
Pierre Halleux <pierre.halleux@acsone.eu>
2025-03-25 14:49:03 +0100

    [MIG] cmis_alf: Migrate to 18.0

--- cmis_web_alf/__manifest__.py
+++ cmis_web_alf/__manifest__.py
@@ -7,7 +7,7 @@
     "category": "Document Management",
     "author": "ACSONE SA/NV",
     "website": "https://alfodoo.org",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["cmis_alf", "cmis_web"],
     "data": [],
